4ccf99deecf740a63b7cfd1e70bd64b915b33691
robertohueso <robertohueso96@gmail.com>
2016-02-22 21:07:02 +0100

    Añade clase Monster
    Revisar Fixme

---
+++ Napakalaki/src/napakalaki/Monster.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package napakalaki;
+
+/**
+ *
+ * @author roberto
+ */
+public class Monster {
+    //Datos miembro
+    private String name;
+    private int combatLevel;
+    private Prize prize;
+    private BadConsequence badCons;
+    
+    //Constructor
+    public Monster(String name, int level, BadConsequence bc, Prize prize){
+        this.name = name;
+        this.combatLevel = level;
+        //FIXME ¿Es necesario constructor de copia?
+        this.prize = prize;
+        this.badCons = bc;
+    }
+    
+    //Getters
+    public String getName(){
+        return this.name;
+    }
+    
+    public int getCombatLevel(){
+        return this.combatLevel;
+    }
+}
