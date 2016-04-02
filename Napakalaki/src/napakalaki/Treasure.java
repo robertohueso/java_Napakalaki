3d5f0e3f521d0aa3dcca63aa70b948cf5b1e2f8e
robertohueso <robertohueso96@gmail.com>
2016-04-02 02:20:01 +0200

    Añade clase Treasure

---
+++ Napakalaki/src/napakalaki/Treasure.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package napakalaki;
+
+/**
+ *
+ * @author roberto
+ */
+public class Treasure {
+    //Variables
+    private String name;
+    private int bonus;
+    private TreasureKind type;
+    
+    //Metodos publicos
+    public Treasure(String n, int bonus, TreasureKind t){
+        
+    }
+    
+    public String getName(){
+        
+    }
+    
+    public int getBonus(){
+        
+    }
+    
+    public int getType(){
+        
+    }
+}
