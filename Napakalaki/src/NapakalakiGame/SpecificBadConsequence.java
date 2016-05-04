51d45de4f40ddad88d788f84a4a8ea9b6818be70
robertohueso <robertohueso96@gmail.com>
2016-05-04 12:27:18 +0200

    Añade nuevas clases

---
+++ Napakalaki/src/NapakalakiGame/SpecificBadConsequence.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package NapakalakiGame;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author roberto
+ */
+public class SpecificBadConsequence extends BadConsequence{
+    //Constructor
+    
+}
